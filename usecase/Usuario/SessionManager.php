6c6cf69ec58799b766ac4d16c2759420339ec477
Yahir2005 <luisyajairo@outlook.com>
2025-10-18 00:37:53 -0600

    UsuarioUseCase

---
+++ usecase/Usuario/SessionManager.php
@@ -0,0 +1,3 @@
+<?php
+ require_once __DIR__ .'/IUsuarioGateway.php';
+ require_once __DIR__ .'/../../Dto/RespuestaGenerica.php';
---
+++ usecase/Usuario/UsuarioUseCase.php
@@ -0,0 +1,29 @@
+<?php
+ require_once __DIR__ .'/IUsuarioGateway.php';
+ require_once __DIR__ .'/../../Dto/RespuestaGenerica.php';
+class UsuarioUseCase{
+    private $gatewayDB;
+    public function __construct(IUsuarioGateway $gatewayDB){
+        $this->gatewayDB = $gatewayDB;
+    }
+
+    public function InsertarUsuario(Usuario $usuario):RespuestaGenerica{
+        $response = new RespuestaGenerica();
+        try {
+            $respuestaMetodo = $this->gatewayDB->InsertarUsuario($usuario);
+            if($respuestaMetodo){
+                $response->status ="ok";
+                $response->body=$respuestaMetodo;
+                $response->message="Registro Exitoso";
+            }else{
+                $response->status="error";
+                $response->body=false;
+                $response->message="Error al registrar";
+            }
+        } catch (Exception $e) {
+            $response->message = $e->getMessage();
+        }
+        return $response;
+    }
+
+}
