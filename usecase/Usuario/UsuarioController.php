d872e5ce7ddf2173317144d1b51b7f37032c20f0
Yahir2005 <luisyajairo@outlook.com>
2025-10-18 00:52:16 -0600

    UsuarioController

---
+++ usecase/Usuario/UsuarioController.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__ .'/UsuarioGatewey.php';
+require_once __DIR__ .'/UsuarioUseCase.php';
+require_once __DIR__ .'/../../Dto/Usuario.php';
+
+class UsuarioController{
+    public function InsertarUsuario(Usuario $usuario):RespuestaGenerica{
+        $gatewayDB = new UsuarioGatewey();
+        $usuarioUseCase = new UsuarioUseCase($gatewayDB);
+        $response = $usuarioUseCase->InsertarUsuario($usuario);
+        return $response;
+
+    }
+}
