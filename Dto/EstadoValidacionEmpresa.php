001fae188a04945b21b0b1e38efe87d4f1727a1f
Yahir2005 <luisyajairo@outlook.com>
2025-11-04 21:32:10 -0600

    DTO EstadoValidacionEmpresa

--- Dto/EstadoValidacionEmpresa.php
+++ Dto/EstadoValidacionEmpresa.php
@@ -2,4 +2,16 @@
 class EstadoValidacionEmpresa{
     private $idEstadoValidacionEmpresa;
     private $estadoValidacionEmpresa;
+
+    public function __construct(){
+
+    }
+
+    public function get($property){
+        return $this->$property;
+    }
+
+    public function set($property, $value){
+        $this->$property = $value;
+    }
 }
