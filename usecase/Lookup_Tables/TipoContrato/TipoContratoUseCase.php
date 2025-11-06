292e44939a38bcf10d5552803ee02d544525853e
K4r0l2013 <garciamdankarol22360910@gmail.com>
2025-11-05 22:52:07 -0600

    ItipocontratoUseCase ListarTipoContrato

---
+++ usecase/Lookup_Tables/TipoContrato/TipoContratoUseCase.php
@@ -0,0 +1,24 @@
+<?php
+require_once _DIR_ ."/../../../Dto/RespuestaGenerica.php";
+
+class TipoContratoUseCase{
+    private $gatewayDB;
+
+    public function __construct(ITipoContrato $gatewayDB){
+        $this->gatewayDB = $gatewayDB;
+    }
+
+    public function listarTipoContrato():RespuestaGenerica{
+        $response = new RespuestaGenerica():
+        $respuestaMetodo = $this ->gatewayDB-> listarTipoContrato();
+        try{
+            $response ->status = "Ok";
+            $response ->body = $respuestaMetodo;
+            $response ->message = "Empresa obtenidas correctamente";
+        } catch (Exception $e)  {
+            $response ->status = "Error";
+            $response ->message = "Error al obtener las empresas: ".$e->getMessage();
+        }
+        return $response;
+    }
+}
