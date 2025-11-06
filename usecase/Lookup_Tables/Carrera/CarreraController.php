d7c521d7d715192c18b602e2461cfe146fde966b
noriegatrejoitzelfabiola-pixel <noriegatrejoitzelfabiola@gmail.com>
2025-11-05 23:29:21 -0600

    CarreraController listarCarrera

--- usecase/Lookup_Tables/Carrera/CarreraController.php
+++ usecase/Lookup_Tables/Carrera/CarreraController.php
@@ -3,9 +3,12 @@ require_once __DIR__ ."/CarreraGateway.php";
 require_once __DIR__ ."/CarreraUseCase.php";
 
 class CarreraController{
-    public function ListarCarrera(): RespuestaGenerica {
+    public function listarCarrera(): RespuestaGenerica {
         $gatewayDB = new CarreraGateway();
         $UseCase = new CarreraUseCase($gatewayDB);
-        return $UseCase->ListarCarrera();
+        return $UseCase->listarCarrera();
     }
     }
+$controller=new CarreraController();
+$$response =$contrroller ->listarCarrera();
+echo $response-> message;
