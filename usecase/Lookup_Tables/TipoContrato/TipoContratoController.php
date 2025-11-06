b0335a59a3bd6c41c6819ce228b2899efd5cd1b1
K4r0l2013 <garciamdankarol22360910@gmail.com>
2025-11-05 23:30:43 -0600

    ItipocontratoController

---
+++ usecase/Lookup_Tables/TipoContrato/TipoContratoController.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__ ."/TipoContratoGateway.php";
+require_once __DIR__ ."/TipoContratoUseCase.php";
+
+class TipoContratoController{
+    public function listarTipoContrato(): RespuestaGenerica {
+        $gatewayDB = new TipoContratoGateway();
+        $UseCase = new TipoContratoUseCase($gatewayDB);
+        return $UseCase->listarTipoContrato();
+    }
+
+}
