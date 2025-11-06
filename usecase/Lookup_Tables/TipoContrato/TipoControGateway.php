97e93ec9e7daf337e7c5adae9bc88fea56b4084d
K4r0l2013 <garciamdankarol22360910@gmail.com>
2025-11-05 23:17:25 -0600

    ItipocontratoGateway

---
+++ usecase/Lookup_Tables/TipoContrato/TipoControGateway.php
@@ -0,0 +1,13 @@
+<?php
+require_once __DIR__ ."/../../DataAccess/MysqlConnector.php";
+require_once __DIR__ ."/ITipoContratoGateway.php";
+
+class TipoContratoGateway implements ITipoContrato{
+
+    public function listarTipoContrato(): array{
+        $mysqlConnector = new MysqlConnector();
+        $sql = "SELECT * FROM TipoContrato";
+        $result = $mysqlConnector->consultaRetorno($sql);
+        return mysqli_fetch_all($result, MYSQLI_ASSOC);
+    }
+}
