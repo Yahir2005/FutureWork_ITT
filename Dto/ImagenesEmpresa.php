6d9e57ed0b1d6b5513603c75290dd9584df22404
Yahir2005 <luisyajairo@outlook.com>
2025-11-07 09:35:07 -0600

    DTO ImagenesEmpresa

---
+++ Dto/ImagenesEmpresa.php
@@ -0,0 +1,18 @@
+<?php
+class ImagenesEmpresa{
+    private $idImagenEmpresa;
+    private $Empresas_idEmpresas;
+    private $urlImagenPerfilEmpresa;
+
+    public function __construct(){
+
+    }
+
+    public function get($property){
+        return $this->$property;
+    }
+
+    public function set($property,$value){
+        $this->$property = $value;
+    }
+}
