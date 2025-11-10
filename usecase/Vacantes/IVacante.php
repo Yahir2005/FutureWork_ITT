f68db5d759e6cc9ef10635aa047f7bf5d8e6ecfd
Yahir2005 <luisyajairo@outlook.com>
2025-11-10 14:42:54 -0600

    Metodos Vacantes

--- usecase/Vacantes/IVacante.php
+++ usecase/Vacantes/IVacante.php
@@ -1,4 +1,12 @@
 <?php
 interface IVacante{
     public function InsertarVacante(Vacantes $vacantes):int;
+
+    public function ActualizarVacante($id, $vacantes):int;
+
+    public function EliminarVacante($id):int;
+
+    public function ListarVacantes():array;
+
+    public function ListarVacantesPorNombre($nombre):array;
 }
