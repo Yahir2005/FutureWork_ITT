f5219f159bf2efbdef87fad4d355cc8fd2c168dd
Yahir2005 <luisyajairo@outlook.com>
2025-11-10 14:21:31 -0600

    VacateGateway InsertarVacante

---
+++ usecase/Vacantes/VacanteGateway.php
@@ -0,0 +1,34 @@
+<?php
+require_once __DIR__ ."/IVacante.php";
+require_once __DIR__ ."/../DataAccess/MysqlConnector.php";
+
+class VacanteGateway implements IVacante{
+    public function InsertarVacante(Vacantes $vacantes):int{
+        $mysqlConnector = new MysqlConnector();
+        $sql = "INSERT INTO vacantes (
+            Empresa_idEmpresa,
+            EstadoValidacionVacante_idEstadoValidacionVacante, 
+            TipoContrato_idTipoContrato, 
+            TipoModalidad_idTipoModalidad,
+            titulo,
+            descripcion,
+            requisitos,
+            ubicacion,
+            salario,
+            fechaLimite
+        ) VALUES (
+            '{$vacantes->get('Empresa_idEmpresa')}',
+            '{$vacantes->get('EstadoValidacionVacante_idEstadoValidacionVacante')}',
+            '{$vacantes->get('TipoContrato_idTipoContrato')}',
+            '{$vacantes->get('TipoModalidad_idTipoModalidad')}',
+            '{$vacantes->get('titulo')}',
+            '{$vacantes->get('descripcion')}',
+            '{$vacantes->get('requisitos')}',
+            '{$vacantes->get('ubicacion')}',
+            '{$vacantes->get('salario')}',
+            '{$vacantes->get('fechaLimite')}'
+        )";
+        $result = $mysqlConnector->consultaSimple($sql);
+        return $result;
+    }
+}
