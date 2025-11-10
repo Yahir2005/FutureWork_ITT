c7d6eb3e41aa86122d770012b2e36a2dc0d7c2d8
Yahir2005 <luisyajairo@outlook.com>
2025-11-10 17:48:34 -0600

    ImagenesPerfilEmpresa Test metodos

--- usecase/Files/ImagenPerfilEmpresa/ImagenesPerfilEmpresaController.php
+++ usecase/Files/ImagenPerfilEmpresa/ImagenesPerfilEmpresaController.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ ."/ImagenesPerfilEmpresaGateway.php";
 require_once __DIR__ ."/ImagenesPerfilEmpresaUseCase.php";
+require_once __DIR__ ."/../../../Dto/ImagenPerfilEmpresa.php";
 
 class ImagenesPerfilEmpresaController{
     public function InsertarImagenPerfilEmpresa(ImagenPerfilEmpresa $imagenPerfilEmpresa):RespuestaGenerica{
@@ -22,3 +23,19 @@ class ImagenesPerfilEmpresaController{
     }
     
 }
+/*
+$controller = new ImagenesPerfilEmpresaController();
+$obj = new ImagenPerfilEmpresa();
+$obj->set("Empresas_idEmpresas",2);
+$obj->set("urlImagenPerfilEmpresa","werrew");
+$result = $controller->InsertarImagenPerfilEmpresa($obj);
+echo $result->message;*/
+/*
+$controller = new ImagenesPerfilEmpresaController();
+$result = $controller->obtenerImagenes(2);
+echo $result->message;
+*/
+/*
+$controller = new ImagenesPerfilEmpresaController();
+$result = $controller->eliminarImagen(2);
+echo $result->message;*/
