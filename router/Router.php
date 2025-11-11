0e3154503943a01360fb84ccc4c6ef97d915c882
Yahir2005 <luisyajairo@outlook.com>
2025-11-10 20:27:26 -0600

    Router usuario

--- router/Router.php
+++ router/Router.php
@@ -3,6 +3,40 @@ require_once __DIR__ .'/../usecase/Usuario/SessionManager.php';
 
 class Router{
     public function CargarVista($vista){
+        switch ($vista) {
+
+        case "Inicio":
+            include __DIR__ .'/../view/InicioView.php';
+        break;
+
+        case 'PerfilPostulante':
+            include __DIR__ .'/../view/PerfilPostulanteView.php';
+        break;
+
+        case 'ṔerfilEmpresa':
+            include __DIR__ .'';
+        break;
+
+        case "Ver_Vacantes":
+            include __DIR__ .'/../view/InicioView.php';
+        break;
+
+        case "Ver_Postulantes":
+            include __DIR__ .'/../view/VerPostulantesView.php';
+        break;
+
+        case "Ver_Vacante":
+            include __DIR__ .'/../view/VerVacanteView.php';
+        break;
+
+        case "Ver_Contacto":
+            include __DIR__ .'/../view/InicioView.php';
+        break;
+
+        case "EXIT":
+                
+        break;
+        }
         
     }
 }
