cb301ec40746cfa115424e367adcf1ab70f77cad
Yahir2005 <luisyajairo@outlook.com>
2025-11-11 13:25:11 -0600

    RouterPostulante

---
+++ router/RouterPostulante.php
@@ -0,0 +1,40 @@
+<?php
+class Router{
+    public function CargarVista($vista){
+        switch ($vista) {
+
+        case "Inicio":
+            include __DIR__ .'/../view/InicioView.php';
+        break;
+
+        case 'PerfilPostulante':
+            include __DIR__ .'/../view/PerfilPostulanteView.php';
+        break;
+
+        case 'ṔerfilEmpresa':
+            include __DIR__ .'';
+        break;
+
+        case "Ver_Vacantes":
+            include __DIR__ .'/../view/InicioView.php';
+        break;
+
+        case "Ver_Postulantes":
+            include __DIR__ .'/../view/VerPostulantesView.php';
+        break;
+
+        case "Ver_Vacante":
+            include __DIR__ .'/../view/VerVacanteView.php';
+        break;
+
+        case "Ver_Contacto":
+            include __DIR__ .'/../view/InicioView.php';
+        break;
+
+        case "EXIT":
+                
+        break;
+        }
+        
+    }
+}
