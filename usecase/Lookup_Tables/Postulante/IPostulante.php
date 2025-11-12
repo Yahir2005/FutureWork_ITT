ce6123e2807b52ea574bbbcd4cb91d343cf62d14
noriegatrejoitzelfabiola-pixel <noriegatrejoitzelfabiola@gmail.com>
2025-11-12 00:08:42 -0600

    Postulante

--- usecase/Lookup_Tables/Postulante/IPostulante.php
+++ usecase/Lookup_Tables/Postulante/IPostulante.php
@@ -1,5 +1,8 @@
 <?php
 interface IPostulante{
     public function listarPostulante ():array;
-    
+    public function listarPostulantePorNombre():array;
+    public function insertarPostulante (Postulante $Postulante):int;
+    public function actualizarPostulante ($id, $Postulante):int; 
+    public function eliminarPostulante ();
 }
