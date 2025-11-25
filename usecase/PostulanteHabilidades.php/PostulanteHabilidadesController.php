edd09d34e4efccbd368ca251bc4a796f35345045
noriegatrejoitzelfabiola-pixel <noriegatrejoitzelfabiola@gmail.com>
2025-11-24 22:03:28 -0600

    PostulanteHabilidades

--- usecase/PostulanteHabilidades.php/PostulanteHabilidadesController.php
+++ usecase/PostulanteHabilidades.php/PostulanteHabilidadesController.php
@@ -3,29 +3,17 @@ require_once __DIR__ ."/PostulanteHabilidadesGatawey.php";
 require_once __DIR__ ."/PostulanteHabilidadesUseCase.php";
 require_once __DIR__ ."/../../Dto/PostulanteHabilidades.php";
  class PostulanteHabilidadesController{
-    public function InsertarPostulanteHabilidades(PostulanteHabilidades $postulanteHabilidades):RespuestaGenerica{
+    public function insertarPostulanteHabilidades(PostulanteHabilidades $postulanteHabilidades):RespuestaGenerica{
         $gatewayDb = new PostulanteHabilidadesGatawey();
         $usecase = new PostulanteHabilidadesUseCase($gatewayDb);
         return $usecase->InsertarPostulanteHabilidades($postulanteHabilidades);
     }
-    public function ActualizarPostulanteHabilidades($id, $postulanteHabilidades):RespuestaGenerica {
-        $gatewayDb = new PostulanteHabilidadesGatawey();
-        $usecase = new PostulanteHabilidadesUseCase($gatewayDb);
-        return $usecase->ActualizarPostulanteHabilidades($id, $postulanteHabilidades);
-    }
-    public function EliminarPostulanteHabilidades($id): RespuestaGenerica{
-        $gatewayDb = new PostulanteHabilidadesGatawey();
-        $usecase = new PostulanteHabilidadesUseCase($gatewayDb);
-        return $usecase->EliminarPostulanteHabilidades($id);
-    }
-    public function ListarPostulanteHabilidades():RespuestaGenerica{
-        $gatewayDb = new PostulanteHabilidadesGatawey();
-        $usecase = new PostulanteHabilidadesUseCase($gatewayDb);
-        return $usecase->ListarPostulanteHabilidades();
-    }
 
  }
- $controller = new PostulanteHabilidadesController();
- $result = $controller -> ListarPostulanteHabilidades();
- echo $result -> message;
+ /*$controller = new PostulanteHabilidadesController();
+ $postulanteHabilidades= new PostulanteHabilidades();
+ $postulanteHabilidades -> set ("Postulante_idPostulante",1);
+ $postulanteHabilidades-> set ("Habilidades_idHabilidad",1);
+ $result = $controller -> insertarPostulanteHabilidades($postulanteHabilidades);
+ echo $result -> message;*/
  
--- usecase/PostulanteHabilidades.php/PostulanteHabilidadesGatawey.php
+++ usecase/PostulanteHabilidades.php/PostulanteHabilidadesGatawey.php
@@ -2,38 +2,17 @@
 require_once __DIR__ ."/IPostulanteHabilidades.php";
 require_once __DIR__ ."/../DataAccess/MysqlConnector.php";
 class PostulanteHabilidadesGatawey implements IPostulanteHabilidades{
- public function InsertarPostulanteHabilidades(PostulanteHabilidades $PostulanteHabilidades):int{
+ public function insertarPostulanteHabilidades(PostulanteHabilidades $postulanteHabilidades):int{
      $mysqlConnector = new MysqlConnector();
         $sql = "INSERT INTO Postulante_Habilidades (
            Postulante_idPostulante,
            Habilidades_idHabilidad
         ) VALUES (
-            '{$PostulanteHabilidades->get('Postulante_idPostulante')}',
-            '{$PostulanteHabilidades->get('Habilidades_idHabilidad')}',
+            '{$postulanteHabilidades->get('Postulante_idPostulante')}',
+            '{$postulanteHabilidades->get('Habilidades_idHabilidad')}',
         )";
         $result = $mysqlConnector->consultaSimple($sql);
         return $result;
     }
-  public function ActualizarPostulanteHabilidades($id, $PostulanteHabilidades):int {
-         $mysqlConnector = new MysqlConnector();
-        $sql = "UPDATE Postulante_Habilidades SET 
-            Postulante_idPostulante = '{$PostulanteHabilidades->get('Postulante_idPostulante')}',
-            Habilidades_idHabilidad = '{$PostulanteHabilidades->get('Habilidades_idHabilidad')}',
-          
-        WHERE idVacante = {$id}";
-        $result = $mysqlConnector->consultaSimple($sql);
-        return $result;
-    }
-  public function EliminarPostulanteHabilidades($id): int{
-        $mysqlConnector = new MysqlConnector();
-        $sql = "DELETE FROM Postulante_Habilidades WHERE idPostulante_Habilidades = {$id}";
-        $result = $mysqlConnector->consultaSimple($sql);
-        return $result;
-    }
-  public function ListarPostulanteHabilidades():array{
-     $mysqlConnector = new MysqlConnector();
-        $sql = "SELECT * FROM Postulante_Habilidades";
-        $result = $mysqlConnector->consultaRetorno($sql);
-        return mysqli_fetch_all($result, MYSQLI_ASSOC);
-    }
+
 }
