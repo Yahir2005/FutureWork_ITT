093bd987126aa7572563c46cd13c0253dcf54aea
Yahir2005 <luisyajairo@outlook.com>
2025-12-01 22:23:12 -0600

    fix navbarPostulante

--- views/viewPostulante/navbarPostulante.php
+++ views/viewPostulante/navbarPostulante.php
@@ -28,10 +28,10 @@
      </div><span class="logo-text">FutureWork ITT</span> </a> <!-- Menú de Navegación -->
     <ul class="navbar-menu">
      <li><a class="nav-link" href="?cargar=Home" >🏠 Inicio</a></li>
-     <li><a class="nav-link" href="?cargar=">💼 Vacantes</a></li>
+     <li><a class="nav-link" href="?cargar=VacantesListView">💼 Vacantes</a></li>
      <li><a class="nav-link" href="?cargar=EmpresasListView">🏢 Empresas</a></li>
      <li><a class="nav-link" href="?cargar=AcercaDeNosotrosView">ℹ️ Nosotros</a></li>
-     <li><a class="nav-link" href="?cargar=">📧 Contacto</a></li>
+     <li><a class="nav-link" href="?cargar=ContactoView">📧 Contacto</a></li>
     </ul><!-- Acciones -->
     <div class="navbar-actions">
      <div class="user-badge">
@@ -40,6 +40,7 @@
       </div><a class="nav-link" href="?cargar=PerfilPostulanteView"><span>Postulante</span></a>
     </div>
    </div>
+  </nav>
 
     <section>
             <?php
