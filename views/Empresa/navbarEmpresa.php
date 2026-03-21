44b75c3c2c76193cf39421dba39c910f9306c8c1
Yahir2005 <luisyajairo@outlook.com>
2026-03-20 21:00:20 -0600

    bootstrap test 1

--- views/Empresa/navbarEmpresa.php
+++ views/Empresa/navbarEmpresa.php
@@ -1,5 +1,5 @@
 <?php
-include_once("/../../router/RouterEmpresa.php");
+include_once __DIR__ . "/../../router/RouterEmpresa.php";
 include_once __DIR__ . "/../../usecase/Usuario/SessionManager.php";
 require_once __DIR__ . "/../../usecase/Usuario/UsuarioController.php";
 
@@ -19,12 +19,40 @@ if(!SessionManager::isUserLoggedIn()){
     <link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.min.css">
 </head>
 <body>
-        <nav class="navbar navbar-light mt-2" style="background-color: rgb(21, 94, 146);">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar Light</a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Mi Empresa</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="?cargar=Home">Inicio</a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="?cargar=Empleados">Empleados</a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Configuración</a>
+                    </li>
+                </ul>
             </div>
-        </nav>
-    </div>
+        </div>
+    </nav>
+
+    <section>
+        <?php
+            $enrutador = new RouterEmpresa();
+            if (isset($_GET['cargar']) && $enrutador->validarGET($_GET['cargar'])) {
+                $enrutador->CargarVista($_GET['cargar']);
+            }
+        ?>
+    </section>
 
     <script src="../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
