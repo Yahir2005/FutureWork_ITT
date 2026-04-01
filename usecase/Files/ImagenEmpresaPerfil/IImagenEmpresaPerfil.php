ccdcff6aa68e56c7e3d59b19d1ff6ebe49494671
Yahir2005 <luisyajairo@outlook.com>
2026-04-01 16:14:42 -0600

    ImagenPerfilEmpresaDTO

---
+++ usecase/Files/ImagenEmpresaPerfil/IImagenEmpresaPerfil.php
@@ -0,0 +1,7 @@
+<?php
+interface IImagenEmpresaPerfil {
+    public function subirImagenPerfilEmpresa(ImagenEmpresaPerfil $imagenData): bool;
+    
+//    public function obtenerImagenPerfilEmpresa($idEmpresa): ?array;
+//    public function eliminarImagenPerfilEmpresa($idEmpresa): int;
+}
