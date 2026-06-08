4a7a4202e4bd49e470547502b323c10438bebdfb
Yahir2005 <luisyajairo@outlook.com>
2026-06-08 15:27:19 -0600

    Vacantes List view

--- usecase/Postulantes/IPostulantes.php
+++ usecase/Postulantes/IPostulantes.php
@@ -1,7 +1,12 @@
 <?php
 interface IPostulantes {
     public function InsertarPostulante(Postulante $postulante): int;
+    
     public function ListarPostulantes(): array;
+
     public function ActualizarPostulante($id, Postulante $postulante): int;
+
     public function EliminarPostulante($id): int;
+
+    public function ObtenerPostulantePorIdUsuario($id): array;
 }
